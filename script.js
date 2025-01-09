8612a51b08e91b19fa88393a2b17f598bd63ee02
tubutubu-hash1 <tubutubu0122@gmail.com>
2025-01-09 16:03:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ function predictMoveByFrequency(moves) {
 }
 
 // コンピュータの手を決定
-function decideComputerMove(moves, order = 3) {
+function decideComputerMove(moves, order = 5) {
     if (moves.length < order) {
         return randomMove();
     }
